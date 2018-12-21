79f9408fc4c2eb60f967386c4647981c1b69424f
Alexandr076 <alexandr076@users.noreply.github.com>
2018-12-21 13:55:16 +0300

    изменена кодировка

--- main.R
+++ main.R
@@ -5,202 +5,203 @@ library("raster")
 library("spatstat")
 source('C:/R/drones/header.R')
 
-{
-# параллельный процесс
-cl <- parallel::makeCluster(4)
-doParallel::registerDoParallel(cl)
-clusterEvalQ(cl, library("retistruct"))
-parallel::stopCluster(cl)
 
-buildingInfo <<- data.frame(fiF = array(NA, N), fiS = array(NA, N), x1 = array(NA, N), y1 = array(NA, N),
-                            x2 = array(NA, N), y2 = array(NA, N), H = H)
-
-{  
-  # # Функция для установки N зданий ручками
-  # PointsForBuilding <- function(N) {
-  #   # Массив для записи координат
-  #   mass <- data.frame(x1 = array(NA, N), y1 = array(NA, N), x2 = array(NA, N), y2 = array(NA, N))
-  #   # foreach(i = 1:N) %do%
-  #   # mass$x1[i] = 
-  #   mass$x1[1] = 7
-  #   mass$y1[1] = 15
-  #   mass$x2[1] = 9
-  #   mass$y2[1] = 20
-  #   mass$x1[2] = 1
-  #   mass$y1[2] = 25
-  #   mass$x2[2] = 15
-  #   mass$y2[2] = 35
-  #   mass$x1[3] = 30
-  #   mass$y1[3] = 2
-  #   mass$x2[3] = 33
-  #   mass$y2[3] = 30
-  #   
-  #   # Вызов функции для записи информации о зданиях
-  #   buildingInfo <- BuildingCreator(mass, APPoint)
-  # }
-  
-  
-  # функция для автоматического формирования зданий для заданной плотности
-  PointsForBuilding <- function(N) {
-    mass <- data.frame(x1 = array(NA, N), y1 = array(NA, N), x2 = array(NA, N), y2 = array(NA, N))
-    # надо доделать окно
-    coreOfCoordinates <- rpoispp(L, 200, win = owin(c(0,40), c(0,40)))
-    foreach(i = 1:N) %do% {
-      
-    }
-  }
-  # Инициализация матрицы с информацией о зданиях с высотой: {fi1, fi2, x1, y1, x2, y2, H}. 
-  # На вход подается начальная координата AP и координаты точек для построек
-  BuildingCreator <- function(M, APPoint) {
-    # Фрейм с данными об объекте buildingInfo
-    # Записываются все данные о здании: две крайние точки прямой и углы между прямыми, соединяющими AP
-      # и эти точки относительно oX
-    for (i in 1:N) {
-      buildingInfo$x1[i] <<- M$x1[i]
-      buildingInfo$y1[i] <<- M$y1[i]
-      buildingInfo$x2[i] <<- M$x2[i]
-      buildingInfo$y2[i] <<- M$y2[i]
-      listWithInfoAboutAngle <- edit_angle(APPoint$x, APPoint$y, M$x1[i], M$y1[i])
-      buildingInfo$fiF[i] <<- listWithInfoAboutAngle$angle
-      listWithInfoAboutAngle <- edit_angle(APPoint$x, APPoint$y, M$x2[i], M$y2[i])
-      buildingInfo$fiS[i] <<- listWithInfoAboutAngle$angle
-    }
-  }
-  
-  
-  # функция проверки, что точка лежит за пределами здания (проверка на fi). На вход подается номер UP 
-    # точки и номер здания. Если пересечение между двумя прямыми, построенными между точками (AP,UP) и 
-      # прямой здания существует, то записывается координата пересечения, иначе - false 
-  VerificationUP <- function(n, N) {
-    coordinates <- line.line.intersection(c(APPoint$x,APPoint$y), c(coordinatesForUP$x[n], 
-                        coordinatesForUP$y[n]), c(buildingInfo$x1[N],buildingInfo$y1[N]),
-                            c(buildingInfo$x2[N],buildingInfo$y2[N]), interior.only = "true")
-    coordinatesForUP$l[n] <<- pointDistance(c(APPoint$x,APPoint$y), 
-                                            c(coordinatesForUP$x[n],coordinatesForUP$y[n]), 
-                                            lonlat = FALSE)
-    if (typeof(coordinates) != "logical") { 
-      # координаты точки пересечения и просчет длин l, l1, l2, проставление флага
-      coordinatesForUP$flag[n] <<- "TRUE"
-      coordinatesForUP$xInter[n] <<- coordinates[1]
-      coordinatesForUP$yInter[n] <<- coordinates[2]
-      coordinatesForUP$l1[n] <<- pointDistance(c(APPoint$x,APPoint$y), 
-                                                c(coordinatesForUP$xInter[n],coordinatesForUP$yInter[n]),
-                                                  lonlat = FALSE)
-      coordinatesForUP$l2[n] <<- pointDistance(c(coordinatesForUP$xInter[n],coordinatesForUP$yInter[n]), 
-                                                c(coordinatesForUP$x[n],coordinatesForUP$y[n]), 
-                                                  lonlat = FALSE)
-    }
-  }
-  
-  # функция для проверки LOS без учета максимального действия AP
-  IsLOS <- function(i, k) {
-    tgAlpha <- (HTx[k] - coordinatesForUP$H[i])/coordinatesForUP$l[i]
-    H_res <- tgAlpha*coordinatesForUP$l2[i] + coordinatesForUP$H[i]
-    if ( H_res > H ) {
-      return(FALSE)
-    }
-    else {
-      return(TRUE)
-    }
-  }
-  
-  # функция для проверки расстояние от AP До UP
-  IsLOSWithoutBuilding <- function(i, k) {
-    d <- sqrt( (HTx[k] - coordinatesForUP$H[i])^2 + (coordinatesForUP$l[i])^2 )
-    if ( d > R) {
-      return(TRUE)
-    }
-    else {
-      return(FALSE)
-    }
-  }
-} 
 {
-  # глобалы: 
-  # n - количество разыгрываемых точек для UP
-  # N - количество построек (зданий)
-  # APPoint - координаты для начальной точки {x, y}
-  # buildingInfo - информационный фрейм об объектах
-  # coordinatesForUP - фрейм для хранения координат пользовательских устройств
-    # рандомной высотой, флагом (FALSE по умолчанию) и координатами точки пересечения
-  # coordinatesForUPWithTrueFlag - тоже самое, что и (выше), но с флагом = TRUE (заблокирован)
-  # UPblockingInfo - информация о зданиях, блокирующих i-й пользовательский девайс 
-    # (в долгосрочной перспективе)
-  # gridSize - размер сетки
-  # R - радиус действия AP
-  # H - высота (для зданий и UP)
-  # HTx - высотка передатчика
-  # w - угол в плоскости YoZ между Tx и Rx
-  # D - расстояние между Tx и Rx в плоскости XoY
-  # l - расстояние между точкой пересечения прямой здания и точкой Tx в плоскости YoZ
-  # pConnect - вероятность los
-  # NumberOfIteration - количество итераций
-  # R - дальность действия передатчика
-  
-  R <<- 35
-  NumberOfIteration <- 50
-  APPoint <<- data.frame(x = 20, y = 20)
-  H <<- 8  # build
-  N <<- 3
-  n <<- 100
-  # не глобал, но определяет минимальную и максимальную высоту передатчика
-  HTxMin <- 12
-  HTxMax <- 30
-  gridSize <<- data.frame(x = 40, y = 40)
-  coordinatesForUP <<- data.frame(x = runif(n, 0, gridSize$x), y = runif(n, 0, gridSize$y),
-                          H = runif(n, 0.01, 2), flag = array(FALSE, n), xInter = array(NA, n),
-                              yInter = array(NA,n), l = array(NA, n), l1 = array(NA, n), l2 = array(NA, n))
-  # кэш
-  coordinatesForUPCache <<- coordinatesForUP
-  # coordinatesForUP <<- data.frame(x = 6, y = 8,
-  #                                 H = 1, flag = array(FALSE, n), xInter = array(NA, n),
-  #                                 yInter = array(NA,n), l = array(NA, n), l1 = array(NA, n), l2 = array(NA, n))
+  # РїР°СЂР°Р»Р»РµР»СЊРЅС‹Р№ РїСЂРѕС†РµСЃСЃ
+  cl <- parallel::makeCluster(4)
+  doParallel::registerDoParallel(cl)
+  clusterEvalQ(cl, library("retistruct"))
+  parallel::stopCluster(cl)
   
   buildingInfo <<- data.frame(fiF = array(NA, N), fiS = array(NA, N), x1 = array(NA, N), y1 = array(NA, N),
-                             x2 = array(NA, N), y2 = array(NA, N), H = H)
+                              x2 = array(NA, N), y2 = array(NA, N), H = H)
   
-  pDisconnect <<- array(NA, NumberOfIteration)
-  pConnect <<- array(NA, NumberOfIteration)
-}
-# clusterExport(cl, varlist = c('buildingInfo','edit_angle',
-#                               '%do%','foreach', 'VerificationUP','determinant',
-#                               'determinant.matrix', 'det','retistruct',
-#                               'line.line.intersection'), envir=environment())
-# par(mfrow=c(2,2))
-
-
-for (k in 1:NumberOfIteration) {
-  coordinatesForUP <<- coordinatesForUPCache
-  # coordinatesForUP[,"x"] <- runif(n, 0, gridSize$x)
-  # coordinatesForUP[,"y"] <- runif(n, 0, gridSize$y)
-  HTx <<- seq(HTxMin, HTxMax, (HTxMax - HTxMin) / (NumberOfIteration - 1))
-  # main
-  # инициация постройки зданий с общей информацией 
-  PointsForBuilding(N)
-  # по всем точкам UP (1:n) проверка для каждого здания (1:N) 
-  foreach(i = 1:n) %do% {
-    foreach(j = 1:N) %do% {
-      VerificationUP(i, j)
+  {  
+    # # Р¤СѓРЅРєС†РёСЏ РґР»СЏ СѓСЃС‚Р°РЅРѕРІРєРё N Р·РґР°РЅРёР№ СЂСѓС‡РєР°РјРё
+    # PointsForBuilding <- function(N) {
+    #   # РњР°СЃСЃРёРІ РґР»СЏ Р·Р°РїРёСЃРё РєРѕРѕСЂРґРёРЅР°С‚
+    #   mass <- data.frame(x1 = array(NA, N), y1 = array(NA, N), x2 = array(NA, N), y2 = array(NA, N))
+    #   # foreach(i = 1:N) %do%
+    #   # mass$x1[i] = 
+    #   mass$x1[1] = 7
+    #   mass$y1[1] = 15
+    #   mass$x2[1] = 9
+    #   mass$y2[1] = 20
+    #   mass$x1[2] = 1
+    #   mass$y1[2] = 25
+    #   mass$x2[2] = 15
+    #   mass$y2[2] = 35
+    #   mass$x1[3] = 30
+    #   mass$y1[3] = 2
+    #   mass$x2[3] = 33
+    #   mass$y2[3] = 30
+    #   
+    #   # Р’С‹Р·РѕРІ С„СѓРЅРєС†РёРё РґР»СЏ Р·Р°РїРёСЃРё РёРЅС„РѕСЂРјР°С†РёРё Рѕ Р·РґР°РЅРёСЏС…
+    #   buildingInfo <- BuildingCreator(mass, APPoint)
+    # }
+    
+    
+    # С„СѓРЅРєС†РёСЏ РґР»СЏ Р°РІС‚РѕРјР°С‚РёС‡РµСЃРєРѕРіРѕ С„РѕСЂРјРёСЂРѕРІР°РЅРёСЏ Р·РґР°РЅРёР№ РґР»СЏ Р·Р°РґР°РЅРЅРѕР№ РїР»РѕС‚РЅРѕСЃС‚Рё
+    PointsForBuilding <- function(N) {
+      mass <- data.frame(x1 = array(NA, N), y1 = array(NA, N), x2 = array(NA, N), y2 = array(NA, N))
+      # РЅР°РґРѕ РґРѕРґРµР»Р°С‚СЊ РѕРєРЅРѕ
+      coreOfCoordinates <- rpoispp(L, 200, win = owin(c(0,40), c(0,40)))
+      foreach(i = 1:N) %do% {
+        
+      }
     }
+    # РРЅРёС†РёР°Р»РёР·Р°С†РёСЏ РјР°С‚СЂРёС†С‹ СЃ РёРЅС„РѕСЂРјР°С†РёРµР№ Рѕ Р·РґР°РЅРёСЏС… СЃ РІС‹СЃРѕС‚РѕР№: {fi1, fi2, x1, y1, x2, y2, H}. 
+    # РќР° РІС…РѕРґ РїРѕРґР°РµС‚СЃСЏ РЅР°С‡Р°Р»СЊРЅР°СЏ РєРѕРѕСЂРґРёРЅР°С‚Р° AP Рё РєРѕРѕСЂРґРёРЅР°С‚С‹ С‚РѕС‡РµРє РґР»СЏ РїРѕСЃС‚СЂРѕРµРє
+    BuildingCreator <- function(M, APPoint) {
+      # Р¤СЂРµР№Рј СЃ РґР°РЅРЅС‹РјРё РѕР± РѕР±СЉРµРєС‚Рµ buildingInfo
+      # Р—Р°РїРёСЃС‹РІР°СЋС‚СЃСЏ РІСЃРµ РґР°РЅРЅС‹Рµ Рѕ Р·РґР°РЅРёРё: РґРІРµ РєСЂР°Р№РЅРёРµ С‚РѕС‡РєРё РїСЂСЏРјРѕР№ Рё СѓРіР»С‹ РјРµР¶РґСѓ РїСЂСЏРјС‹РјРё, СЃРѕРµРґРёРЅСЏСЋС‰РёРјРё AP
+      # Рё СЌС‚Рё С‚РѕС‡РєРё РѕС‚РЅРѕСЃРёС‚РµР»СЊРЅРѕ oX
+      for (i in 1:N) {
+        buildingInfo$x1[i] <<- M$x1[i]
+        buildingInfo$y1[i] <<- M$y1[i]
+        buildingInfo$x2[i] <<- M$x2[i]
+        buildingInfo$y2[i] <<- M$y2[i]
+        listWithInfoAboutAngle <- edit_angle(APPoint$x, APPoint$y, M$x1[i], M$y1[i])
+        buildingInfo$fiF[i] <<- listWithInfoAboutAngle$angle
+        listWithInfoAboutAngle <- edit_angle(APPoint$x, APPoint$y, M$x2[i], M$y2[i])
+        buildingInfo$fiS[i] <<- listWithInfoAboutAngle$angle
+      }
+    }
+    
+    
+    # С„СѓРЅРєС†РёСЏ РїСЂРѕРІРµСЂРєРё, С‡С‚Рѕ С‚РѕС‡РєР° Р»РµР¶РёС‚ Р·Р° РїСЂРµРґРµР»Р°РјРё Р·РґР°РЅРёСЏ (РїСЂРѕРІРµСЂРєР° РЅР° fi). РќР° РІС…РѕРґ РїРѕРґР°РµС‚СЃСЏ РЅРѕРјРµСЂ UP 
+    # С‚РѕС‡РєРё Рё РЅРѕРјРµСЂ Р·РґР°РЅРёСЏ. Р•СЃР»Рё РїРµСЂРµСЃРµС‡РµРЅРёРµ РјРµР¶РґСѓ РґРІСѓРјСЏ РїСЂСЏРјС‹РјРё, РїРѕСЃС‚СЂРѕРµРЅРЅС‹РјРё РјРµР¶РґСѓ С‚РѕС‡РєР°РјРё (AP,UP) Рё 
+    # РїСЂСЏРјРѕР№ Р·РґР°РЅРёСЏ СЃСѓС‰РµСЃС‚РІСѓРµС‚, С‚Рѕ Р·Р°РїРёСЃС‹РІР°РµС‚СЃСЏ РєРѕРѕСЂРґРёРЅР°С‚Р° РїРµСЂРµСЃРµС‡РµРЅРёСЏ, РёРЅР°С‡Рµ - false 
+    VerificationUP <- function(n, N) {
+      coordinates <- line.line.intersection(c(APPoint$x,APPoint$y), c(coordinatesForUP$x[n], 
+                                                                      coordinatesForUP$y[n]), c(buildingInfo$x1[N],buildingInfo$y1[N]),
+                                            c(buildingInfo$x2[N],buildingInfo$y2[N]), interior.only = "true")
+      coordinatesForUP$l[n] <<- pointDistance(c(APPoint$x,APPoint$y), 
+                                              c(coordinatesForUP$x[n],coordinatesForUP$y[n]), 
+                                              lonlat = FALSE)
+      if (typeof(coordinates) != "logical") { 
+        # РєРѕРѕСЂРґРёРЅР°С‚С‹ С‚РѕС‡РєРё РїРµСЂРµСЃРµС‡РµРЅРёСЏ Рё РїСЂРѕСЃС‡РµС‚ РґР»РёРЅ l, l1, l2, РїСЂРѕСЃС‚Р°РІР»РµРЅРёРµ С„Р»Р°РіР°
+        coordinatesForUP$flag[n] <<- "TRUE"
+        coordinatesForUP$xInter[n] <<- coordinates[1]
+        coordinatesForUP$yInter[n] <<- coordinates[2]
+        coordinatesForUP$l1[n] <<- pointDistance(c(APPoint$x,APPoint$y), 
+                                                 c(coordinatesForUP$xInter[n],coordinatesForUP$yInter[n]),
+                                                 lonlat = FALSE)
+        coordinatesForUP$l2[n] <<- pointDistance(c(coordinatesForUP$xInter[n],coordinatesForUP$yInter[n]), 
+                                                 c(coordinatesForUP$x[n],coordinatesForUP$y[n]), 
+                                                 lonlat = FALSE)
+      }
+    }
+    
+    # С„СѓРЅРєС†РёСЏ РґР»СЏ РїСЂРѕРІРµСЂРєРё LOS Р±РµР· СѓС‡РµС‚Р° РјР°РєСЃРёРјР°Р»СЊРЅРѕРіРѕ РґРµР№СЃС‚РІРёСЏ AP
+    IsLOS <- function(i, k) {
+      tgAlpha <- (HTx[k] - coordinatesForUP$H[i])/coordinatesForUP$l[i]
+      H_res <- tgAlpha*coordinatesForUP$l2[i] + coordinatesForUP$H[i]
+      if ( H_res > H ) {
+        return(FALSE)
+      }
+      else {
+        return(TRUE)
+      }
+    }
+    
+    # С„СѓРЅРєС†РёСЏ РґР»СЏ РїСЂРѕРІРµСЂРєРё СЂР°СЃСЃС‚РѕСЏРЅРёРµ РѕС‚ AP Р”Рѕ UP
+    IsLOSWithoutBuilding <- function(i, k) {
+      d <- sqrt( (HTx[k] - coordinatesForUP$H[i])^2 + (coordinatesForUP$l[i])^2 )
+      if ( d > R) {
+        return(TRUE)
+      }
+      else {
+        return(FALSE)
+      }
+    }
+  } 
+  {
+    # РіР»РѕР±Р°Р»С‹: 
+    # n - РєРѕР»РёС‡РµСЃС‚РІРѕ СЂР°Р·С‹РіСЂС‹РІР°РµРјС‹С… С‚РѕС‡РµРє РґР»СЏ UP
+    # N - РєРѕР»РёС‡РµСЃС‚РІРѕ РїРѕСЃС‚СЂРѕРµРє (Р·РґР°РЅРёР№)
+    # APPoint - РєРѕРѕСЂРґРёРЅР°С‚С‹ РґР»СЏ РЅР°С‡Р°Р»СЊРЅРѕР№ С‚РѕС‡РєРё {x, y}
+    # buildingInfo - РёРЅС„РѕСЂРјР°С†РёРѕРЅРЅС‹Р№ С„СЂРµР№Рј РѕР± РѕР±СЉРµРєС‚Р°С…
+    # coordinatesForUP - С„СЂРµР№Рј РґР»СЏ С…СЂР°РЅРµРЅРёСЏ РєРѕРѕСЂРґРёРЅР°С‚ РїРѕР»СЊР·РѕРІР°С‚РµР»СЊСЃРєРёС… СѓСЃС‚СЂРѕР№СЃС‚РІ
+    # СЂР°РЅРґРѕРјРЅРѕР№ РІС‹СЃРѕС‚РѕР№, С„Р»Р°РіРѕРј (FALSE РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ) Рё РєРѕРѕСЂРґРёРЅР°С‚Р°РјРё С‚РѕС‡РєРё РїРµСЂРµСЃРµС‡РµРЅРёСЏ
+    # coordinatesForUPWithTrueFlag - С‚РѕР¶Рµ СЃР°РјРѕРµ, С‡С‚Рѕ Рё (РІС‹С€Рµ), РЅРѕ СЃ С„Р»Р°РіРѕРј = TRUE (Р·Р°Р±Р»РѕРєРёСЂРѕРІР°РЅ)
+    # UPblockingInfo - РёРЅС„РѕСЂРјР°С†РёСЏ Рѕ Р·РґР°РЅРёСЏС…, Р±Р»РѕРєРёСЂСѓСЋС‰РёС… i-Р№ РїРѕР»СЊР·РѕРІР°С‚РµР»СЊСЃРєРёР№ РґРµРІР°Р№СЃ 
+    # (РІ РґРѕР»РіРѕСЃСЂРѕС‡РЅРѕР№ РїРµСЂСЃРїРµРєС‚РёРІРµ)
+    # gridSize - СЂР°Р·РјРµСЂ СЃРµС‚РєРё
+    # R - СЂР°РґРёСѓСЃ РґРµР№СЃС‚РІРёСЏ AP
+    # H - РІС‹СЃРѕС‚Р° (РґР»СЏ Р·РґР°РЅРёР№ Рё UP)
+    # HTx - РІС‹СЃРѕС‚РєР° РїРµСЂРµРґР°С‚С‡РёРєР°
+    # w - СѓРіРѕР» РІ РїР»РѕСЃРєРѕСЃС‚Рё YoZ РјРµР¶РґСѓ Tx Рё Rx
+    # D - СЂР°СЃСЃС‚РѕСЏРЅРёРµ РјРµР¶РґСѓ Tx Рё Rx РІ РїР»РѕСЃРєРѕСЃС‚Рё XoY
+    # l - СЂР°СЃСЃС‚РѕСЏРЅРёРµ РјРµР¶РґСѓ С‚РѕС‡РєРѕР№ РїРµСЂРµСЃРµС‡РµРЅРёСЏ РїСЂСЏРјРѕР№ Р·РґР°РЅРёСЏ Рё С‚РѕС‡РєРѕР№ Tx РІ РїР»РѕСЃРєРѕСЃС‚Рё YoZ
+    # pConnect - РІРµСЂРѕСЏС‚РЅРѕСЃС‚СЊ los
+    # NumberOfIteration - РєРѕР»РёС‡РµСЃС‚РІРѕ РёС‚РµСЂР°С†РёР№
+    # R - РґР°Р»СЊРЅРѕСЃС‚СЊ РґРµР№СЃС‚РІРёСЏ РїРµСЂРµРґР°С‚С‡РёРєР°
+    
+    R <<- 35
+    NumberOfIteration <- 50
+    APPoint <<- data.frame(x = 20, y = 20)
+    H <<- 8  # build
+    N <<- 3
+    n <<- 100
+    # РЅРµ РіР»РѕР±Р°Р», РЅРѕ РѕРїСЂРµРґРµР»СЏРµС‚ РјРёРЅРёРјР°Р»СЊРЅСѓСЋ Рё РјР°РєСЃРёРјР°Р»СЊРЅСѓСЋ РІС‹СЃРѕС‚Сѓ РїРµСЂРµРґР°С‚С‡РёРєР°
+    HTxMin <- 12
+    HTxMax <- 30
+    gridSize <<- data.frame(x = 40, y = 40)
+    coordinatesForUP <<- data.frame(x = runif(n, 0, gridSize$x), y = runif(n, 0, gridSize$y),
+                                    H = runif(n, 0.01, 2), flag = array(FALSE, n), xInter = array(NA, n),
+                                    yInter = array(NA,n), l = array(NA, n), l1 = array(NA, n), l2 = array(NA, n))
+    # РєСЌС€
+    coordinatesForUPCache <<- coordinatesForUP
+    # coordinatesForUP <<- data.frame(x = 6, y = 8,
+    #                                 H = 1, flag = array(FALSE, n), xInter = array(NA, n),
+    #                                 yInter = array(NA,n), l = array(NA, n), l1 = array(NA, n), l2 = array(NA, n))
+    
+    buildingInfo <<- data.frame(fiF = array(NA, N), fiS = array(NA, N), x1 = array(NA, N), y1 = array(NA, N),
+                                x2 = array(NA, N), y2 = array(NA, N), H = H)
+    
+    pDisconnect <<- array(NA, NumberOfIteration)
+    pConnect <<- array(NA, NumberOfIteration)
   }
-  # координаты для UP с флагом true
-  # coordinatesForUPWithTrueFlag <<- coordinatesForUP[coordinatesForUP[,"flag"] == TRUE,]
-  # для каждой заблокированной точки вычисляем H и усправляем флаг, если нужно
-  for (i in 1:length(coordinatesForUP[,1])) {
-    if ( (coordinatesForUP$flag[i] == TRUE) && IsLOS(i, k) == FALSE) {
-      coordinatesForUP$flag[i] <- FALSE
-    }
-    if (IsLOSWithoutBuilding(i, k) == TRUE) {
-      coordinatesForUP$flag[i] <- TRUE
-    }
-  }
+  # clusterExport(cl, varlist = c('buildingInfo','edit_angle',
+  #                               '%do%','foreach', 'VerificationUP','determinant',
+  #                               'determinant.matrix', 'det','retistruct',
+  #                               'line.line.intersection'), envir=environment())
+  # par(mfrow=c(2,2))
   
-  # вычисляем вероятность успешного коннекта
-  pDisconnect[k] <- length(coordinatesForUP[coordinatesForUP[,"flag"] == TRUE,1])/n
-
-
-  # графики
-  # if (k %% 12 == 0) {
+  
+  for (k in 1:NumberOfIteration) {
+    coordinatesForUP <<- coordinatesForUPCache
+    # coordinatesForUP[,"x"] <- runif(n, 0, gridSize$x)
+    # coordinatesForUP[,"y"] <- runif(n, 0, gridSize$y)
+    HTx <<- seq(HTxMin, HTxMax, (HTxMax - HTxMin) / (NumberOfIteration - 1))
+    # main
+    # РёРЅРёС†РёР°С†РёСЏ РїРѕСЃС‚СЂРѕР№РєРё Р·РґР°РЅРёР№ СЃ РѕР±С‰РµР№ РёРЅС„РѕСЂРјР°С†РёРµР№ 
+    PointsForBuilding(N)
+    # РїРѕ РІСЃРµРј С‚РѕС‡РєР°Рј UP (1:n) РїСЂРѕРІРµСЂРєР° РґР»СЏ РєР°Р¶РґРѕРіРѕ Р·РґР°РЅРёСЏ (1:N) 
+    foreach(i = 1:n) %do% {
+      foreach(j = 1:N) %do% {
+        VerificationUP(i, j)
+      }
+    }
+    # РєРѕРѕСЂРґРёРЅР°С‚С‹ РґР»СЏ UP СЃ С„Р»Р°РіРѕРј true
+    # coordinatesForUPWithTrueFlag <<- coordinatesForUP[coordinatesForUP[,"flag"] == TRUE,]
+    # РґР»СЏ РєР°Р¶РґРѕР№ Р·Р°Р±Р»РѕРєРёСЂРѕРІР°РЅРЅРѕР№ С‚РѕС‡РєРё РІС‹С‡РёСЃР»СЏРµРј H Рё СѓСЃРїСЂР°РІР»СЏРµРј С„Р»Р°Рі, РµСЃР»Рё РЅСѓР¶РЅРѕ
+    for (i in 1:length(coordinatesForUP[,1])) {
+      if ( (coordinatesForUP$flag[i] == TRUE) && IsLOS(i, k) == FALSE) {
+        coordinatesForUP$flag[i] <- FALSE
+      }
+      if (IsLOSWithoutBuilding(i, k) == TRUE) {
+        coordinatesForUP$flag[i] <- TRUE
+      }
+    }
+    
+    # РІС‹С‡РёСЃР»СЏРµРј РІРµСЂРѕСЏС‚РЅРѕСЃС‚СЊ СѓСЃРїРµС€РЅРѕРіРѕ РєРѕРЅРЅРµРєС‚Р°
+    pDisconnect[k] <- length(coordinatesForUP[coordinatesForUP[,"flag"] == TRUE,1])/n
+    
+    
+    # РіСЂР°С„РёРєРё
+    # if (k %% 12 == 0) {
     plot(APPoint$x[3], APPoint$y[4], xlim = c(0,40), ylim = c(0,40), col = "black", pch = 19, xlab = 'x', ylab = 'y')
     lines(x = c(buildingInfo$x1[1],buildingInfo$x2[1]), y = c(buildingInfo$y1[1],buildingInfo$y2[1]))
     lines(x = c(buildingInfo$x1[2],buildingInfo$x2[2]), y = c(buildingInfo$y1[2],buildingInfo$y2[2]))
@@ -209,12 +210,12 @@ for (k in 1:NumberOfIteration) {
            coordinatesForUP$y[coordinatesForUP[,"flag"] == TRUE], pch = 19, col = "red")
     points(coordinatesForUP$x[coordinatesForUP[,"flag"] == FALSE],
            coordinatesForUP$y[coordinatesForUP[,"flag"] == FALSE], pch = 19, col = "green")
-  # }
+    # }
+  }
+  
 }
 
-}
-
-# на всякий случай, вдруг пригодится
+# РЅР° РІСЃСЏРєРёР№ СЃР»СѓС‡Р°Р№, РІРґСЂСѓРі РїСЂРёРіРѕРґРёС‚СЃСЏ
 # points(coordinatesForUP$x[9], 
 #        coordinatesForUP$y[9], pch = 19, col = "red")
 # points(coordinatesForUP$x[1], coordinatesForUP$y[1], col = 'green', pch = 19)
