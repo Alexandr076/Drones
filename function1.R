ac7488b2e72a4c1a44f6081b3d2b98d00e1d9100
Alexandr076 <36568075+Alexandr076@users.noreply.github.com>
2018-12-21 18:16:47 +0300

    Add files via upload

---
+++ function1.R
@@ -0,0 +1,100 @@
+# функция для автоматического формирования зданий для заданной плотности
+PointsForBuilding <- function(lmbd, Lmin, Lmax, mass, BuildNumber) {
+  foreach(i = 1:BuildNumber) %do% {
+    mass$L[i] <- runif(1, Lmin/2, Lmax/2)
+    mass$angle[i] <- runif(1, 0, 2*pi)
+    if (mass$angle[i] > pi/2) && (mass$angle[i] < pi) {
+      mass$x1[i] = coreOfCoordinates$x[i] - (mass$L[i] * cos(pi - mass$angle[i]))
+      mass$y1[i] = coreOfCoordinates$y[i] + (mass$L[i] * sin(pi - mass$angle[i]))
+      mass$x2[i] = coreOfCoordinates$x[i] + (mass$L[i] * cos(pi - mass$angle[i]))
+      mass$y2[i] = coreOfCoordinates$y[i] - (mass$L[i] * sin(pi - mass$angle[i]))
+    }
+    if ( (mass$angle[i] == 0) || (mass$angle[i] == pi) ) {
+      mass$x1[i] = coreOfCoordinates$x[i] + mass$L[i]
+      mass$y1[i] = coreOfCoordinates$y[i]
+      mass$x2[i] = coreOfCoordinates$x[i] - mass$L[i]
+      mass$y2[i] = coreOfCoordinates$y[i]
+    }
+    if (mass$angle[i] == pi/2) {
+      mass$x1[i] = coreOfCoordinates$x[i]
+      mass$y1[i] = coreOfCoordinates$y[i] + mass$L[i]
+      mass$x2[i] = coreOfCoordinates$x[i] 
+      mass$y2[i] = coreOfCoordinates$y[i] - mass$L[i]
+    }
+    else {
+      mass$x1[i] = coreOfCoordinates$x[i] + (mass$L[i] * cos(mass$angle[i]))
+      mass$y1[i] = coreOfCoordinates$y[i] - (mass$L[i] * sin(mass$angle[i]))
+      mass$x2[i] = coreOfCoordinates$x[i] - (mass$L[i] * cos(mass$angle[i]))
+      mass$y2[i] = coreOfCoordinates$y[i] + (mass$L[i] * sin(mass$angle[i]))
+    }
+  }
+}
+
+# Инициализация матрицы с информацией о зданиях с высотой: {fi1, fi2, x1, y1, x2, y2, H}. 
+# На вход подается начальная координата AP и координаты точек для построек
+
+BuildingCreator <- function(M, APPoint) {
+  # Фрейм с данными об объекте buildingInfo
+  # Записываются все данные о здании: две крайние точки прямой и углы между прямыми, соединяющими AP
+  # и эти точки относительно oX
+  for (i in 1:N) {
+    buildingInfo$x1[i] <- M$x1[i]
+    buildingInfo$y1[i] <- M$y1[i]
+    buildingInfo$x2[i] <- M$x2[i]
+    buildingInfo$y2[i] <- M$y2[i]
+    listWithInfoAboutAngle <- edit_angle(APPoint$x, APPoint$y, M$x1[i], M$y1[i])
+    buildingInfo$fiF[i] <- listWithInfoAboutAngle$angle
+    listWithInfoAboutAngle <- edit_angle(APPoint$x, APPoint$y, M$x2[i], M$y2[i])
+    buildingInfo$fiS[i] <- listWithInfoAboutAngle$angle
+  }
+}
+
+
+# функция проверки, что точка лежит за пределами здания (проверка на fi). На вход подается номер UP 
+# точки и номер здания. Если пересечение между двумя прямыми, построенными между точками (AP,UP) и 
+# прямой здания существует, то записывается координата пересечения, иначе - false 
+
+VerificationUP <- function(n, N) {
+  coordinates <- line.line.intersection(c(APPoint$x,APPoint$y), c(coordinatesForUP$x[n], 
+                                                                  coordinatesForUP$y[n]), c(buildingInfo$x1[N],buildingInfo$y1[N]),
+                                        c(buildingInfo$x2[N],buildingInfo$y2[N]), interior.only = "true")
+  coordinatesForUP$l[n] <- pointDistance(c(APPoint$x,APPoint$y), 
+                                          c(coordinatesForUP$x[n],coordinatesForUP$y[n]), 
+                                          lonlat = FALSE)
+  if (typeof(coordinates) != "logical") { 
+    # координаты точки пересечения и просчет длин l, l1, l2, проставление флага
+    coordinatesForUP$flag[n] <- "TRUE"
+    coordinatesForUP$xInter[n] <- coordinates[1]
+    coordinatesForUP$yInter[n] <- coordinates[2]
+    coordinatesForUP$l1[n] <- pointDistance(c(APPoint$x,APPoint$y), 
+                                             c(coordinatesForUP$xInter[n],coordinatesForUP$yInter[n]),
+                                             lonlat = FALSE)
+    coordinatesForUP$l2[n] <- pointDistance(c(coordinatesForUP$xInter[n],coordinatesForUP$yInter[n]), 
+                                             c(coordinatesForUP$x[n],coordinatesForUP$y[n]), 
+                                             lonlat = FALSE)
+  }
+}
+
+# функция для проверки LOS без учета максимального действия AP
+
+IsLOS <- function(i, k) {
+  tgAlpha <- (HTx[k] - coordinatesForUP$H[i])/coordinatesForUP$l[i]
+  H_res <- tgAlpha*coordinatesForUP$l2[i] + coordinatesForUP$H[i]
+  if ( H_res > H ) {
+    return(FALSE)
+  }
+  else {
+    return(TRUE)
+  }
+}
+
+# функция для проверки расстояние от AP До UP
+IsLOSWithoutBuilding <- function(i, k) {
+  d <- sqrt( (HTx[k] - coordinatesForUP$H[i])^2 + (coordinatesForUP$l[i])^2 )
+  if ( d > R) {
+    return(TRUE)
+  }
+  else {
+    return(FALSE)
+  }
+}
---
+++ main1.R
@@ -0,0 +1,105 @@
+library("foreach")
+library("retistruct")
+library("doParallel")
+library("raster")
+library("spatstat")
+source('C:/R/drones/header.R')
+
+
+# глобалы: 
+# Lmin - минимально возможная длина здания
+# Lmax - максимально возможная длина здания
+# R - дальность действия передатчика
+# NumberOfIteration - количество итераций
+# APPoint - координаты для начальной точки {x, y}
+# H - высота (для зданий и UP)
+# n - количество разыгрываемых точек для UP
+# HTxMin - минимальная высота дрона
+# HTxMax - максимальная высота дрона
+# gridSize - размер сетки
+# coordinatesForUP - фрейм для хранения координат пользовательских устройств
+	# с указанием высоты, флагом (FALSE по умолчанию) и координатами точки пересечения
+# lmbd - простность для распределения зданий 
+# coreOfCoordinates - центральная координата для зданий
+# coordinatesForUPWithTrueFlag - тоже самое, что и (выше), но с флагом = TRUE (заблокирован)
+# BuildNumber - количество зданий
+# buildingInfo - информационный фрейм об объектах
+# pDisconnect - вероятность дисконнекта
+# pConnect - вероятность los
+
+
+# HTx - высотка передатчика
+# angle - угол в плоскости YoZ между Tx и Rx
+# D - расстояние между Tx и Rx в плоскости XoY
+
+
+Lmin <- 15
+Lmax <- 15
+R <- 35
+NumberOfIteration <- 50
+APPoint <- data.frame(x = 20, y = 20)
+HBuild <- 8  
+n <- 100
+HTxMin <- 30
+HTxMax <- 30
+gridSize <- data.frame(x = 40, y = 40)
+coordinatesForUP <- data.frame(x = runif(n, 0, gridSize$x), y = runif(n, 0, gridSize$y),
+                                H = runif(n, 0.01, 2), flag = array(FALSE, n), xInter = array(NA, n),
+                                yInter = array(NA, n), l = array(NA, n), l1 = array(NA, n), l2 = array(NA, n))
+lmbd <- 10^-3
+coreOfCoordinates <- rpoispp(lmbd, win = owin(c(0,40), c(0,40)))
+BuildNumber <- coreOfCoordinates$n
+buildingInfo <- data.frame(x1 = array(NA, BuildNumber), y1 = array(NA, BuildNumber), x2 = array(NA, BuildNumber), 
+                            y2 = array(NA, BuildNumber), H = HBuild, L = array(NA, BuildNumber), angle = array(NA, BuildNumber))
+pDisconnect <- array(NA, NumberOfIteration)
+pConnect <- array(NA, NumberOfIteration)
+
+# основная программа
+for (k in 1:NumberOfIteration) {
+	lmbd <- k * 10^-3
+	HTx <- array(HTx, dim = NumberOfIteration)
+	# инициация постройки зданий с общей информацией
+	PointsForBuilding(lmbd, Lmin, Lmax, buildingInfo, BuildNumber)
+	foreach(i = 1:n) %do% {
+	  foreach(j = 1:BuildNumber) %do% {
+	    VerificationUP(i, j)
+	  }
+	}
+	# координаты для UP с флагом true
+	# coordinatesForUPWithTrueFlag <- coordinatesForUP[coordinatesForUP[,"flag"] == TRUE,]
+	# для каждой заблокированной точки вычисляем H и усправляем флаг, если нужно
+	for (i in 1:length(coordinatesForUP[,1])) {
+	  if ( (coordinatesForUP$flag[i] == TRUE) && IsLOS(i, k) == FALSE) {
+	    coordinatesForUP$flag[i] <- FALSE
+	  }
+	  if (IsLOSWithoutBuilding(i, k) == TRUE) {
+	    coordinatesForUP$flag[i] <- TRUE
+	  }
+	}
+
+	# вычисляем вероятность успешного коннекта
+	pDisconnect[k] <- length(coordinatesForUP[coordinatesForUP[,"flag"] == TRUE,1])/n
+
+
+	# графики
+	# if (k %% 12 == 0) {
+	plot(APPoint$x[3], APPoint$y[4], xlim = c(0,40), ylim = c(0,40), col = "black", pch = 19, xlab = 'x', ylab = 'y')
+	lines(x = c(buildingInfo$x1[1],buildingInfo$x2[1]), y = c(buildingInfo$y1[1],buildingInfo$y2[1]))
+	lines(x = c(buildingInfo$x1[2],buildingInfo$x2[2]), y = c(buildingInfo$y1[2],buildingInfo$y2[2]))
+	lines(x = c(buildingInfo$x1[3],buildingInfo$x2[3]), y = c(buildingInfo$y1[3],buildingInfo$y2[3]))
+	points(coordinatesForUP$x[coordinatesForUP[,"flag"] == TRUE],
+	       coordinatesForUP$y[coordinatesForUP[,"flag"] == TRUE], pch = 19, col = "red")
+	points(coordinatesForUP$x[coordinatesForUP[,"flag"] == FALSE],
+	       coordinatesForUP$y[coordinatesForUP[,"flag"] == FALSE], pch = 19, col = "green")
+	# }
+}
+  
+
+
+# на всякий случай, вдруг пригодится
+# points(coordinatesForUP$x[9], 
+#        coordinatesForUP$y[9], pch = 19, col = "red")
+# points(coordinatesForUP$x[1], coordinatesForUP$y[1], col = 'green', pch = 19)
+# points(coordinatesForUP$x[15], coordinatesForUP$y[15], col = 'red', pch = 19)
+
+
